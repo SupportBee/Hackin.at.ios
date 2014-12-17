7a6f782ee7af65460af9720e5fb348d6797396ef
Prateek Dayal <prateek@supportbee.com>
2014-12-17 12:08:51 -0800

    a view controller for posting new broadcast and a button to show it

---
+++ Hackin.at/NewBroadcastViewController.swift
@@ -0,0 +1,14 @@
+//
+//  NewBroadcastViewController.swift
+//  Hackin.at
+//
+//  Created by Prateek on 12/17/14.
+//  Copyright (c) 2014 Prateek Dayal. All rights reserved.
+//
+
+import UIKit
+
+class NewBroadcastViewController: UIViewController {
+    
+    
+}
