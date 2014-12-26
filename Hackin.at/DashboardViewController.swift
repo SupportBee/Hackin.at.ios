0d3c87378ad008ade81b9995dac55d1347b22bb4
Prateek Dayal <prateek@supportbee.com>
2014-12-26 08:47:15 -0800

    moving the broadcast button to navbar and adding a tableview for listing broadcasts

--- Hackin.at/DashboardViewController.swift
+++ Hackin.at/DashboardViewController.swift
@@ -27,14 +27,14 @@ class DashboardViewController: UIViewController {
        // Do any additional setup after loading the view.
         welcomeLabel.text = "Welcome to Hackin.at \(login)"
     }
-    
-   
    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func broadcastButtonPressed(sender: AnyObject) {
+    }
     /*
     // MARK: - Navigation
     
