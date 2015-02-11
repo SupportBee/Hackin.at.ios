ffc0fa1dc0b9bbc7bcad3badd7154b3984432beb
Avinasha Shastry <me@avinasha.com>
2015-02-11 19:49:21 +0530

    Adds BroadcastListing which handles the BroadcastTableView

--- Hackin.at/BroadcastsViewController.swift
+++ Hackin.at/BroadcastsViewController.swift
@@ -17,96 +17,48 @@
 import UIKit
 import PureLayout
 
-class BroadcastTableViewDataSource:NSObject, UITableViewDataSource{
-    var broadcasts: Array<Broadcast> = []
-    
-    func fetchBroadcasts(#success: () -> ()){
-        func onFetch(broadcasts: [Broadcast]){
-            self.broadcasts = broadcasts
-            success()
-        }
-        Broadcast.fetchBroadcasts(success: onFetch)
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.broadcasts.count;
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier(
-            "BroadcastCell", forIndexPath:indexPath) as BroadcastTableViewCell
-        let broadcast = broadcasts[indexPath.row]
-        cell.setupViewData(broadcast)
-        return cell
-    }
-}
-
 class BroadcastsViewController: UIViewController, UITableViewDelegate {
     
-    @IBOutlet weak var broadcastsTableView: UITableView!
-    
     var broadcastTableViewDataSource = BroadcastTableViewDataSource()
+    var broadcastListing: BroadcastListing!
     var tableRefreshControl:TableRefreshControl!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupTableViewStyle()
-        setupTableViewWiring()
-        setupAutoRefresh()
-        fetchBroadcasts()
-    }
-    
-    func setupTableViewWiring(){
-        self.broadcastsTableView.delegate = self
-        self.broadcastsTableView.dataSource = broadcastTableViewDataSource
-        self.broadcastsTableView.registerNib(
-            UINib(nibName:"BroadcastTableViewCell", bundle:nil), forCellReuseIdentifier: "BroadcastCell")
-    }
-    
-    func setupAutoRefresh(){
-        tableRefreshControl = TableRefreshControl.setupForTableViewWithAction(
-            tableView: self.broadcastsTableView,
-            target: self,
-            action: "refreshBroadcasts"
-        )
-    }
-    
-    func setupTableViewStyle(){
-        self.broadcastsTableView.estimatedRowHeight = 100
-        self.broadcastsTableView.rowHeight = UITableViewAutomaticDimension
-        self.broadcastsTableView.separatorInset = UIEdgeInsetsZero
-    }
-    
-    override func updateViewConstraints() {
-        self.broadcastsTableView.autoPinToTopLayoutGuideOfViewController(self, withInset: 0)
-        self.broadcastsTableView.autoPinToBottomLayoutGuideOfViewController(self, withInset: 0)
-        self.broadcastsTableView.autoPinEdgeToSuperviewEdge(ALEdge.Right)
-        self.broadcastsTableView.autoPinEdgeToSuperviewEdge(ALEdge.Left)
-        super.updateViewConstraints()
-    }
-    
-    func refreshBroadcasts(){
-        fetchBroadcasts(success: { self.tableRefreshControl.endRefreshing() })
-    }
-    
-    func fetchBroadcasts(success: () -> () = {}){
-        broadcastTableViewDataSource.fetchBroadcasts(success: {
-            self.broadcastsTableView.reloadData()
-            success()
-        })
-    }
     
     @IBAction func broadcastButtonPressed(sender: AnyObject) {
         var newBroadcastStoryBoard = UIStoryboard(name: "NewBroadcast", bundle: nil)
         let vc = newBroadcastStoryBoard.instantiateViewControllerWithIdentifier("newBroadcastViewController") as UINavigationController;
         self.presentViewController(vc, animated: true, completion: nil)
     }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupListing()
+        initListing()
+    }
+    
+    override func updateViewConstraints() {
+        self.broadcastListing.autoPinToTopLayoutGuideOfViewController(self, withInset: 0)
+        self.broadcastListing.autoPinToBottomLayoutGuideOfViewController(self, withInset: 0)
+        self.broadcastListing.autoPinEdgeToSuperviewEdge(ALEdge.Right)
+        self.broadcastListing.autoPinEdgeToSuperviewEdge(ALEdge.Left)
+        super.updateViewConstraints()
+    }
+    
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         var newBroadcastStoryBoard = UIStoryboard(name: "Broadcasts", bundle: nil)
         let vc = newBroadcastStoryBoard.instantiateViewControllerWithIdentifier("broadcastViewController") as BroadcastViewController;
         vc.broadcast = broadcastTableViewDataSource.broadcasts[indexPath.row]
         self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    
+    private func setupListing(){
+        broadcastListing = BroadcastListing(tableViewDataSource: broadcastTableViewDataSource, tableViewDelegate: self, pullToRefresh: true)
+        self.view.addSubview(broadcastListing)
+    }
+    
+    private func initListing(){
+        broadcastListing.refresh()
     }
     
     override func didReceiveMemoryWarning() {
