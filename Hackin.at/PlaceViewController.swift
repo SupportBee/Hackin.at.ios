1bee7d3e64576dc5647caa797618091676144c73
Prateek Dayal <prateek@supportbee.com>
2015-02-11 19:48:14 +0530

    a simple map view for the place

--- Hackin.at/PlaceViewController.swift
+++ Hackin.at/PlaceViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MapKit
 
 class PlaceViewController: UIViewController {
     
@@ -16,16 +17,32 @@ class PlaceViewController: UIViewController {
     @IBOutlet weak var latLongLabel: UILabel!
     @IBOutlet weak var distanceLabel: UILabel!
     
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         
         let name = place!.name
         let location = place!.location
-
-        println("At \(place) \(name) \(location)")
+        let lat = (location.latitude as NSString).doubleValue
+        let lng = (location.longitude as NSString).doubleValue
         
+        let coords = CLLocationCoordinate2D(
+            latitude: lat,
+            longitude: lng
+        )
+        
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: coords, span: span)
+        mapView.setRegion(region, animated: true)
+
         nameLabel.text = name
         latLongLabel.text = "\(location.longitude),\(location.latitude)"
         
+        let annotation = MKPointAnnotation()
+        annotation.setCoordinate(coords)
+        annotation.title = name
+        mapView.addAnnotation(annotation)
+
     }
 
 }
