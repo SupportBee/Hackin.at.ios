121be61c7586564b9e78c57c54fac09986704d58
Prateek Dayal <prateek@supportbee.com>
2015-02-12 10:56:32 +0530

    refactoring some code. Can't get mapview to auto-layout

--- Hackin.at/PlaceViewController.swift
+++ Hackin.at/PlaceViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import PureLayout
 
 class PlaceViewController: UIViewController {
     
@@ -15,13 +16,18 @@ class PlaceViewController: UIViewController {
     
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var latLongLabel: UILabel!
-    @IBOutlet weak var distanceLabel: UILabel!
     
     @IBOutlet weak var mapView: MKMapView!
     
+    var name = ""
+    
     override func viewDidLoad() {
-        
-        let name = place!.name
+        name = place!.name
+        nameLabel.text = name
+        mapView.layer
+    }
+    
+    override func viewDidAppear(animated: Bool) {
         let location = place!.location
         let lat = (location.latitude as NSString).doubleValue
         let lng = (location.longitude as NSString).doubleValue
@@ -35,14 +41,23 @@ class PlaceViewController: UIViewController {
         let region = MKCoordinateRegion(center: coords, span: span)
         mapView.setRegion(region, animated: true)
 
-        nameLabel.text = name
-        latLongLabel.text = "\(location.longitude),\(location.latitude)"
         
         let annotation = MKPointAnnotation()
         annotation.setCoordinate(coords)
         annotation.title = name
         mapView.addAnnotation(annotation)
+    }
+    
+    override func updateViewConstraints() {
+        nameLabel.autoPinToTopLayoutGuideOfViewController(self, withInset: AppTheme.Listing.elementsPadding)
+        nameLabel.autoPinEdgeToSuperviewEdge(ALEdge.Left, withInset: AppTheme.HackerListing.paddingLeft)
 
+        // NOTE: Can't style maps using PureLayout. Doing it in IB
+        
+        //mapView.autoPinEdgeToSuperviewEdge(ALEdge.Left, withInset: AppTheme.HackerListing.paddingLeft)
+        //mapView.autoPinEdgeToSuperviewEdge(ALEdge.Right, withInset: AppTheme.HackerListing.paddingLeft)
+        //mapView.autoPinEdge(ALEdge.Top, toEdge: ALEdge.Bottom, ofView: nameLabel, withOffset: AppTheme.HackerListing.paddingTop)
+        super.updateViewConstraints()
     }
 
 }
