ef27dd83c4247822426ab8df8e7d54ec71b019f9
Prateek Dayal <prateek@supportbee.com>
2015-03-04 17:28:12 +0530

    a blank friends view controller

---
+++ Hackin.at/FriendsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FriendsViewController.swift
+//  Hackin.at
+//
+//  Created by Prateek on 3/4/15.
+//  Copyright (c) 2015 Prateek Dayal. All rights reserved.
+//
+
+
+import UIKit
+import PureLayout
+
+class FriendsViewController: UINavigationController {
+    
+    override func viewDidLoad() {
+        println("Friends View Controller")
+    }
+    
+}
