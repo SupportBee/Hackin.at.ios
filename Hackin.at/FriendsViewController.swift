c67320556700e00b76557aba1a0fbbbeb0784b21
Prateek Dayal <prateek@supportbee.com>
2015-03-05 15:12:30 +0530

    HackerListingView works without any configuration options

--- Hackin.at/FriendsViewController.swift
+++ Hackin.at/FriendsViewController.swift
@@ -10,24 +10,24 @@
 import UIKit
 import PureLayout
 
-class FriendsViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource {
+class FriendsViewController: UIViewController, UISearchBarDelegate {
     
-    @IBOutlet var hackersTableView: UITableView!
-    var hackers: Array<Hacker> = []
-    var tableRefreshControl:TableRefreshControl!
+    var friendsListing: HackersListingView!
     
     override func viewDidLoad() {
         setupSearchBar()
         setupFriendsListing()
-  //      setupTableViewStyle()
-  //      setupTableViewWiring()
-  //      setupAutoRefresh()
-  //      fetchFriends()
     }
     
     func setupFriendsListing(){
-        let friendsListing = HackersListingView()
+        friendsListing = HackersListingView()
         view.addSubview(friendsListing)
+        friendsListing.fetchFriends()
+    }
+    
+    override func updateViewConstraints() {
+        friendsListing.autoPinEdgesToSuperviewEdgesWithInsets(UIEdgeInsetsZero)
+        super.updateViewConstraints()
     }
     
     func setupSearchBar(){
@@ -44,70 +44,6 @@ class FriendsViewController: UIViewController, UISearchBarDelegate, UITableViewD
         
     }
     
-    func setupTableViewWiring(){
-        self.hackersTableView.delegate = self
-        self.hackersTableView.dataSource = self
-        self.hackersTableView.registerNib(UINib(nibName: "HackerTableViewCell", bundle: nil), forCellReuseIdentifier: "HackerCell")
-    }
-    
-    func setupAutoRefresh(){
-        tableRefreshControl = TableRefreshControl.setupForTableViewWithAction(
-            tableView: self.hackersTableView,
-            target: self,
-            action: "refreshHackers"
-        )
-    }
-    
-    func setupTableViewStyle(){
-        self.hackersTableView.estimatedRowHeight = 100
-        self.hackersTableView.rowHeight = UITableViewAutomaticDimension
-        self.hackersTableView.separatorInset = UIEdgeInsetsZero
-    }
-    
-    override func updateViewConstraints() {
-        self.hackersTableView.autoPinToTopLayoutGuideOfViewController(self, withInset: 0)
-        self.hackersTableView.autoPinToBottomLayoutGuideOfViewController(self, withInset: 0)
-        self.hackersTableView.autoPinEdgeToSuperviewEdge(ALEdge.Right)
-        self.hackersTableView.autoPinEdgeToSuperviewEdge(ALEdge.Left)
-        super.updateViewConstraints()
-    }
-    
-    func refreshHackers(){
-        func onFetch(hackers:[Hacker]){
-            renderHackers(hackers)
-            self.tableRefreshControl.endRefreshing()
-        }
-        CurrentHacker().friends(success: onFetch)
-    }
-    
-    func fetchFriends(){
-        CurrentHacker().friends(success: renderHackers)
-    }
-    
-    func renderHackers(hackers:[Hacker]){
-        println("Render \(hackers.count) Hackers")
-        self.hackers = hackers
-        self.hackersTableView.reloadData()
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return hackers.count
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("HackerCell", forIndexPath: indexPath) as HackerTableViewCell
-        
-        let hacker = self.hackers[indexPath.row]
-        cell.setupViewData(hacker)
-        return cell
-    }
-    
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let hacker = self.hackers[indexPath.row]
-        var hackersStoryboard = UIStoryboard(name: "Hackers", bundle: nil);
-        let vc = hackersStoryboard.instantiateViewControllerWithIdentifier("profileViewController") as ProfileViewController
-        vc.hacker = hacker
-        self.navigationController?.pushViewController(vc, animated: true)
-    }
+
     
 }
