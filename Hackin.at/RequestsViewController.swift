a64a1aad6b7034917547be60f6db8166e99e14e4
Prateek Dayal <prateek@supportbee.com>
2015-03-14 10:45:36 +0530

    [RequestView] Adding a tab item and controller for requests screen

---
+++ Hackin.at/RequestsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RequestsViewController.swift
+//  Hackin.at
+//
+//  Created by Prateek on 3/14/15.
+//  Copyright (c) 2015 Prateek Dayal. All rights reserved.
+//
+
+import UIKit
+
+class RequestsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        println("Requests View Controller")
+    }
+}
