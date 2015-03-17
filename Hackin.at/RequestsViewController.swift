670cfa9a528e1056712b42de8f35646ca95d74b5
Prateek Dayal <prateek@supportbee.com>
2015-03-17 12:30:55 +0530

    [RequestsView] Listing hackers that have sent the friendship requests - no buttons yet

--- Hackin.at/RequestsViewController.swift
+++ Hackin.at/RequestsViewController.swift
@@ -8,18 +8,62 @@
 
 import UIKit
 
-class RequestsViewController: UIViewController {
+class RequestsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var requests: [FriendshipRequest] = []
+    var requestsTable: UITableView!
     
     override func viewDidLoad() {
+        setupRequestsTable()
         fetchMyFriendshipRequests()
+    }
+    
+    func setupRequestsTable(){
+        self.requestsTable = UITableView()
+        requestsTable.dataSource = self
+        requestsTable.delegate = self
+        requestsTable.registerClass(UITableViewCell.self, forCellReuseIdentifier: "RequestCell")
+        view.addSubview(requestsTable)
+     
     }
     
     func fetchMyFriendshipRequests(){
         
         func onFetch(requests: [FriendshipRequest]){
-            println("Found \(requests.count) requests")
+            self.requests = requests
+            renderRequests()
         }
         
         FriendshipRequest.all(onFetch)
+    }
+    
+    func renderRequests(){
+        println("render requests")
+       requestsTable.reloadData()
+    }
+
+    override func updateViewConstraints() {
+        requestsTable.autoPinEdgesToSuperviewEdgesWithInsets(UIEdgeInsetsZero)
+        super.updateViewConstraints()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        println("render requests \(requests.count)")
+        return requests.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = requestsTable.dequeueReusableCellWithIdentifier("RequestCell") as UITableViewCell
+        let hacker = self.requests[indexPath.row].sender
+        cell.textLabel?.text  = "@\(hacker.login)"
+        
+        hacker.fetchAvatarURL(size: CGFloat(48.0), success: {
+            (url: String) in
+            cell.imageView!.sd_setImageWithURL(NSURL(string: url), placeholderImage: UIImage(named: "logo_square.png"))
+        })
+        
+        
+        cell.accessoryView = SendFriendshipRequestButton(toBeFriend: hacker)
+        return cell;
     }
 }
