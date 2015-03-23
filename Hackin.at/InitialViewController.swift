d1f237470fe224461c77eb7132205a87cd12b667
Avinasha Shastry <me@avinasha.com>
2015-03-23 12:02:44 +0530

    Refactors InitialViewController for code readability

--- Hackin.at/InitialViewController.swift
+++ Hackin.at/InitialViewController.swift
@@ -9,12 +9,11 @@
 import UIKit
 import PureLayout
 
-// All Globals here for now
 class InitialViewController: UIViewController, LoginViewDelegate {
     
     @IBOutlet weak var logoImageView: UIImageView!
-    // Location setup
     
+   
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -27,6 +26,15 @@ class InitialViewController: UIViewController, LoginViewDelegate {
         }
     }
     
+    override func updateViewConstraints() {
+        logoImageView.autoAlignAxisToSuperviewAxis(ALAxis.Horizontal)
+        logoImageView.autoAlignAxisToSuperviewAxis(ALAxis.Vertical)
+        super.updateViewConstraints()
+    }
+    
+    
+    
+    
     func showLoginView(){
         let vc = self.storyboard?.instantiateViewControllerWithIdentifier("loginViewController") as LoginViewController;
         vc.delegate = self
@@ -36,15 +44,22 @@ class InitialViewController: UIViewController, LoginViewDelegate {
     func hackerLoggedIn() {
         self.dismissViewControllerAnimated(true, completion: nil)
         
-        trackLogin()
-        Hackinat.sharedInstance.resetAlamofireManager()
-        setHackerDeviceToken()
-        
+        onLogin()
         postLoginInit()
     }
     
     func postLoginInit(){
         launchApp()
+    }
+    
+    func onLogin(){
+        resetAPIWrapper()
+        trackLogin()
+        setHackerDeviceToken()
+    }
+    
+    func resetAPIWrapper(){
+        Hackinat.sharedInstance.resetAlamofireManager()
     }
     
     func trackLogin(){
@@ -56,14 +71,8 @@ class InitialViewController: UIViewController, LoginViewDelegate {
         let vc = self.storyboard?.instantiateViewControllerWithIdentifier("mainViewController") as MainViewController;
         self.presentViewController(vc, animated: true, completion: nil)
     }
-    
-    override func updateViewConstraints() {
-        logoImageView.autoAlignAxisToSuperviewAxis(ALAxis.Horizontal)
-        logoImageView.autoAlignAxisToSuperviewAxis(ALAxis.Vertical)
-        super.updateViewConstraints()
-    }
  
-    private func setHackerDeviceToken(){
+    func setHackerDeviceToken(){
         let deviceAPNSToken = NSUserDefaults.standardUserDefaults().objectForKey("apns_device_token") as? String
         if(deviceAPNSToken == nil){ return }
         
