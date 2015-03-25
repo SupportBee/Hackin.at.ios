b7ff3eb357d0677f89725673ce3dfb68b23ba40c
Avinasha Shastry <me@avinasha.com>
2015-03-25 11:40:44 +0530

    Removes code which handles Interactive Actions

--- Hackin.at/AppDelegate.swift
+++ Hackin.at/AppDelegate.swift
@@ -48,17 +48,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         PushNotificationHandler.handle(pushNotificationData)
     }
     
-    func application(application: UIApplication, handleActionWithIdentifier identifier: String?, forRemoteNotification userInfo: [NSObject : AnyObject], completionHandler: () -> Void) {
-
-        let pushNotificationData = Helpers.Transformers.userInfoToPushData(userInfo)
-        let apnsData = PushNotificationManager.pushManager().getApnPayload(userInfo)
-        var apnsCategory = apnsData["category"] as String?
-        
-        PushNotificationActionsHandler.handle(identifier, category: apnsCategory, data: pushNotificationData)
-        
-        completionHandler()
-    }
-
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
